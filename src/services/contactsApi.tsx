62a3d5325ca44e85a1b9a6055690bc80037c7096
Annie Huang <annie.hn@gmail.com>
2022-02-13 14:37:27 +1100

    Add tagTypes/providesTags/invalidateTags in createApi and confirm after adding a new entry, the item will appear in <Home> without refreshing the page.

--- src/services/contactsApi.tsx
+++ src/services/contactsApi.tsx
@@ -4,11 +4,14 @@ import { Contact } from '../model/contact.model';
 export const contactsApi = createApi({
   reducerPath: 'contactsApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+  // An array of string tag type names. Specifying tag types is optional, but you should define them so that they can be used for caching and invalidation. When defining a tag type, you will be able to provide them with providesTags and invalidate them with invalidatesTags when configuring endpoints.
+  tagTypes: ['Contact'],
   endpoints: (builder) => ({
     contacts: builder.query<Contact[], void>({
       // Test error
       // query: () => '/conta',
       query: () => '/contacts',
+      providesTags: ['Contact'],
     }),
     addContact: builder.mutation<{}, Contact>({
       query: (contact) => ({
@@ -16,6 +19,7 @@ export const contactsApi = createApi({
         method: 'POST',
         body: contact,
       }),
+      invalidatesTags: ['Contact'],
     }),
     deleteContact: builder.mutation<void, string>({
       query: (id) => ({
