7339f5a5f8e450ae158fa4b60ba3aafce00aa041
Annie Huang <annie.hn@gmail.com>
2022-02-13 13:01:03 +1100

    Create a boilerplate for form in AddEdit.tsx

--- src/pages/AddEdit.tsx
+++ src/pages/AddEdit.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './AddEdit.css';
 
+const initialState = {
+  name: '',
+  email: '',
+  contact: '',
+};
+
 const AddEdit = () => {
+  const [formValue, setFormValue] = useState(initialState);
+  const { name, email, contact } = formValue;
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {};
+  const handleSubmit = (e) => {};
+
   return (
-    <div>
-      <h2>AddEdit</h2>
+    <div style={{ marginTop: '100px' }}>
+      <form
+        style={{
+          margin: 'auto',
+          padding: '15px',
+          maxWidth: '400px',
+          alignContent: 'center',
+        }}
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor='name'>Name</label>
+        <input
+          type='text'
+          name='name'
+          id='name'
+          placeholder='Enter Name...'
+          value={name}
+          onChange={handleInputChange}
+        />
+      </form>
     </div>
   );
 };
