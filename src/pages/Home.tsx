5ac7ed9ffad303e804b3c2a1e832719b4e24a348
Annie Huang <annie.hn@gmail.com>
2022-02-13 12:21:09 +1100

    Add "Add Contact" button into Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useContactsQuery } from '../services/contactsApi';
 
 const Home = () => {
+  // isFetching - When true, indicates that the query is currently fetching, but might have data from an earlier request. This will be true for both the first request fired off, as well as subsequent requests.
+  // isLoading - When true, indicates that the query is currently loading for the first time, and has no data yet. This will be true for the first request fired off, but not for subsequent requests.
+  const { data, isLoading, error } = useContactsQuery();
+
   return (
-    <div>
-      <h2>Home</h2>
+    <div style={{ marginTop: '100px' }}>
+      <Link to='/addContact'>
+        <button className='btn btn-add'>Add Contact</button>
+      </Link>
+      <br />
+      <br />
     </div>
   );
 };
