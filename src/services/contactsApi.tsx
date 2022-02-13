1dc0216a6d4a44fda6f24dcbdddb401087b2a8b9
Annie Huang <annie.hn@gmail.com>
2022-02-13 12:45:44 +1100

    Add a 'addContact' endpoint for contactsApi and export the useAddContactMutation hook

--- src/services/contactsApi.tsx
+++ src/services/contactsApi.tsx
@@ -7,10 +7,18 @@ export const contactsApi = createApi({
   endpoints: (builder) => ({
     contacts: builder.query<Contact[], void>({
       // Test error
-      query: () => '/conta',
-      // query: () => '/contacts',
+      // query: () => '/conta',
+      query: () => '/contacts',
+    }),
+    addContact: builder.mutation<{}, Contact>({
+      query: (contact) => ({
+        url: '/contacts',
+        method: 'POST',
+        body: contact,
+      }),
     }),
   }),
 });
 
-export const { useContactsQuery } = contactsApi;
+// other examples I follow just export contactsApi and get the hook in the component files that use it.
+export const { useContactsQuery, useAddContactMutation } = contactsApi;
