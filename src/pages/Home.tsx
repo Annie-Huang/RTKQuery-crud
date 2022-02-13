20e56549859a158edd32b3238b97421beef3336d
Annie Huang <annie.hn@gmail.com>
2022-02-13 12:32:21 +1100

    Add the loading message and verified it worked.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { useContactsQuery } from '../services/contactsApi';
 
 const Home = () => {
   // isFetching - When true, indicates that the query is currently fetching, but might have data from an earlier request. This will be true for both the first request fired off, as well as subsequent requests.
   // isLoading - When true, indicates that the query is currently loading for the first time, and has no data yet. This will be true for the first request fired off, but not for subsequent requests.
   const { data, isLoading, error } = useContactsQuery();
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
 
   return (
     <div style={{ marginTop: '100px' }}>
